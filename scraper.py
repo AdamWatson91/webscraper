8fc9f43566c48c18f57dec13b1b67370082d8b6c
Adam Watson <adamwatson91@gmail.com>
2022-02-22 21:38:16 +0000

    Add functionality to write scraped data for each page for each individual folder with json file.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from logging import root
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -6,17 +7,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
+from itertools import groupby
+from urllib.parse import urlparse, parse_qs
 import requests
 import time
 import random
 import uuid
+import os
+import json
+from pathlib import Path
+
 # research the below options and more and decide whther to implement
 # options = webdriver.ChromeOptions()
 # options.add_argument('start-maximized')
 # options.add_argument('disable-infobars')
 # options.add_argument('--disable-extensions')
-# print('this will always run')
-# if __name__ == "__main__":
+
 class Scraper:
     def __init__(self, url, options=None):
         self.url = url
@@ -68,20 +74,38 @@ class Scraper:
         return data
 
     def scrape_multiple_page_elements(self, **kwargs):
-        Scraped_dict = kwargs
+        scraped_dict = kwargs
         
         for k, v in kwargs.items():
             data = self.driver.find_elements(By.XPATH, v)
             data = [i.text for i in data]
-            Scraped_dict[k].append(data)
-        return Scraped_dict
+            scraped_dict[k].append(data)
+        return scraped_dict
+    
+    def scrape_multiple_page_elements_v2(self, **kwargs):
+        scraped_dict = {}
+        for k, v in kwargs.items():
+            v_element = self.driver.find_elements(By.XPATH, v)
+            v_element = [i.text for i in v_element]
+            scraped_dict[k] = v_element
+        return scraped_dict
+        # going to need to then append for each link
+        # for key,val in kwarg.items():
+        #   if key in d:
+        #     scraped_dict[key] = [scraped_dict[key],val]
+            
+        
+        #for v in scaped_dict:
+        #     data = self.driver.find_elements(By.XPATH, kwargs.values())
+        #     data = [i.text for i in data]
+        #     scraped_dict[v].append(data)
+
+        # return scraped_dict
     
     def testing_kwargs(self, **kwargs):
         print(kwargs)
         print(kwargs.items())
         print(kwargs.values())
-
-        
 
     def navigate_to(self, xpath, link_tag):
         link = self.driver.find_element(By.XPATH, xpath)
@@ -143,11 +167,9 @@ class Scraper:
             sign_in_button.click()
 
     def generate_uid(self):
-        seed = random.getrandbits(32)
-        while True:
-            yield seed
-            self.uid = seed
-            seed += 1
+        parsed = urlparse(self.url)
+        query = parse_qs(parsed.query)
+        [page] = query['page']
     
     def generate_uuid4(self):
         self.uuid_foir = uuid.uuid4()
@@ -156,31 +178,51 @@ class Scraper:
         img = self.driver.find_elements(By.XPATH, xpath).text
         # img = [i.text for i in data]
         return img
+    
+    def create_directory(self,directory_name, directory_path ):
+            # identify the root of the path
+        self.directory_name = directory_name
+        self.directory_path = directory_path
+        self.path = os.path.join(self.directory_path, self.directory_name)
+        # make the directory in the root of the project
+        try:
+            os.makedirs(self.path, exist_ok=True)
+        except OSError:
+            print ("Creation of the directory %s failed" % self.path)
+        else:
+            print ("Successfully created the directory %s " % self.path)
+
 
 
 if __name__ == "__main__":
     URL = 'https://www.allrecipes.com/search/results/?search='
     bot  = Scraper(URL)
-    #bot.testing_kwargs(ingredient_list_multi='//span[@class="ingredients-item-name elementFont__body"]', recipe_meta_multi='//div[@class="recipe-meta-item"]')
     bot.accept_cookies('//*[@id="onetrust-accept-btn-handler"]',None)
     #bot.navigate_to('//a[@class="card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"]', 'href')
-    
-    #print(ingredient_list)
-    #print(recipe_meta)
-    #scrape_page = bot.scrape_page_elements(ingredient_list_multi='//span[@class="ingredients-item-name elementFont__body"]',recipe_meta_multi='//div[@class="recipe-meta-item"]')
-    #print(scrape_page)
-    
-    links = bot.scrape_page_links('//a[@class="card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"]', 1)
-    
-    recipe_dict = {
-                    'link': [],    
-                    'ingredient_list': [],
-                    'recipe_meta': []
-                    }
-    
+    #scaped_dict = bot.scrape_multiple_page_elements_v2(ingredients_list_multi='//span[@class="ingredients-item-name elementFont__body"]', recipe_meta='//div[@class="recipe-meta-item"]')
+    #print(scaped_dict)
+
+    links = bot.scrape_page_links('//a[@class="card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"]',1)
+    # scraped_dict = {}
+    # for link in links:
+    #     bot.driver.get(link)
+    #     time.sleep(2)
+    #     scaped_page_dict = bot.scrape_multiple_page_elements_v2(ingredients_list_multi='//span[@class="ingredients-item-name elementFont__body"]', recipe_meta='//div[@class="recipe-meta-item"]')
+    #     # scraped_dict.update(scraped_dict)
+    # print(scraped_dict)
+    bot.create_directory('raw_data',os.getcwd())
+
     for link in links:
+        recipe_dict = {
+                'recipe_id': [],
+                'link': [],    
+                'ingredient_list': [],
+                'recipe_meta': []
+                }
         bot.driver.get(link)
         time.sleep(2)
+        recipe_id = [int(''.join(group)) for key, group in groupby(iterable=link, key=lambda e: e.isdigit()) if key]
+        recipe_dict['recipe_id'].append(recipe_id)
         recipe_dict['link'].append(link)
         try:
             ingredient_list = bot.scrape_page_elements('//span[@class="ingredients-item-name elementFont__body"]')
@@ -194,13 +236,14 @@ if __name__ == "__main__":
         except NoSuchElementException:
             print('No Element found')
             recipe_dict['recipe_meta'].append('N/A')
-    
+        bot.create_directory(str(recipe_id), os.path.join(os.getcwd(),'raw_data'))
+        with open(os.path.join(os.getcwd(),'raw_data',str(recipe_id) ,'data.json'), mode='w') as f:
+            json.dump(recipe_dict, f)
     print(recipe_dict)
+
     #main_image = bot.download_image('//div[@class="component lazy-image lazy-image-udf lead-media ugc-photos-link aspect_3x2 rendered image-loaded"]')
     #print(main_image)
-    # #bot.perform_search_with_bar('//*[@id="primary-search"]', 'some text')
-    # bot.scroll_page()
-    # bot.scroll_infinite()
-    # print(links)
+
+
     # path for the main image //div[@class='component lazy-image lazy-image-udf lead-media ugc-photos-link aspect_3x2 rendered image-loaded']
 
