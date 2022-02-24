aebd33903b2d278ce0e76507cc1adafc0bd35230
Adam Watson <adamwatson91@gmail.com>
2022-02-24 21:42:29 +0000

    Add final refactor for code at this point. Fix the uuid4 issue. Add doc strings for methods.

--- scraper.py
+++ scraper.py
@@ -26,17 +26,49 @@ from pathlib import Path
 
 #REFACTORING ENHANCEMENTS:
 # 1. Review the functions and note where functions do the same thing and package this into a function and call it. E.g. i have many find_elements throughout
-
+# 2. Check whether i have static methods here
 class Scraper:
+    """
+    This class provides the main functionality required to webscrape data using Chrome Webdriver.
+
+    Args:
+        url (str) : The url to begin the web scarper
+        options (str): The desired web driver options to the user desires
+            (default is None)
+    """
     def __init__(self, url, options=None):
+        """
+        See help(Scraper) for accurate signature.
+        """
         self.url = url
         if options:
             self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         else:
             self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(self.url)
+    
+    def create_json(self, path, file_name):
+        with open(os.path.join(path,file_name), mode='w') as f:
+            json.dump(recipe_dict, f)
+        time.sleep(1)
+
+    def get_root_path(self):
+        root_path = os.getcwd()
+        return root_path
+    
+    def extract_continous_digit_group(self, iterable_lelement):
+        digit_group = [int(''.join(group)) for key, group in groupby(iterable=iterable_lelement, key=lambda e: e.isdigit()) if key]
+        return str(digit_group)
 
     def accept_cookies(self, xpath, iframe=None):
+        """
+        This function will click an accept cookies button.
+
+        Args:
+            xpath (str): Xpath link for the accept cookies button which will be clicked once found.
+            iframe (str): Xpath link for the iframe with the accept cookie button
+                (default is None)
+        """
         time.sleep(2)
         try:
             self.driver.switch_to(iframe)
@@ -47,28 +79,64 @@ class Scraper:
             accept_cookies_button = self.driver.find_element(By.XPATH, xpath)
             accept_cookies_button.click()
             time.sleep(2)
-        # except:
-        #     print('No cookies button clicked with with xpath provided')
+        else:
+            print('No cookies button clicked with xpath provided')
     
     def perform_search_with_bar(self, xpath, text):
+        """
+        This function will type and search for the inputted text 
+        using the websites search bar by pressing Enter.
+
+        Args:
+            xpath (str): Xpath link for the search bar.
+            text (str): The text that will be searched for.
+        """
         search_bar = self.driver.find_element(By.XPATH, xpath)
         search_bar.clear()
         search_bar.send_keys(text)
-        time.sleep(2) # Sleep for to monitor testing
+        time.sleep(1) # Sleep to monitor testing
 
-    # def perform_search_with_link(self, xpath, text):
-    #     search_bar = self.find_element(By.XPATH, xpath)
-    #     search_bar = search_bar.send_keys()
     def scrape_element(self, xpath):
+        """
+        This function will find and scrape and convert to text
+        a single element.
+
+        Args:
+            xpath (str): Xpath link for the element to be scraped.
+        Returns:
+            str: a string representing the text in the specified element
+        """
         data = self.driver.find_element(By.XPATH, xpath).text
         return data
 
     def scrape_page_elements(self, xpath):
+        """
+        This function will find and scrape a list of elements. 
+        Each list element will be converted to text.
+
+        Args:
+            xpath (str): Xpath link for the element to be scraped.
+                Returns:
+            list: a list of string representing the text in the specified elements list.
+        """
         data = self.driver.find_elements(By.XPATH, xpath)
         data = [i.text for i in data]
         return data
     
     def scrape_multiple_page_elements(self, **kwargs):
+        """
+        This function creates scrapes n elements input by the user and
+        stored them in a dictionary, ready for JSON.
+
+        User can use .update() to add to an existing
+
+        Args:
+            kwargs (dict): The key should be the name for scraped element and 
+                            the value should be the xpath for the element.
+        Returns:
+            dict: a dictionary representing the key(s) name specified with the a list
+                    of strings for each item in the the specified elements list.
+        """
         scraped_dict = {}
         for k, v in kwargs.items():
             try:
@@ -81,24 +149,44 @@ class Scraper:
         return scraped_dict
 
     def navigate_to(self, xpath, link_tag):
+        """
+        This function finds and clicks a link based on an xpath.
+
+        Args:
+            xpath (str): Xpath link for the link to be clicked.
+            link_tag (str): The attribute with the hyperlink, usuall 'href'.
+        """
         link = self.driver.find_element(By.XPATH, xpath)
         link = link.get_attribute(link_tag)
         self.driver.get(link)
-        time.sleep(3)
+        time.sleep(1)
     
     def scroll_page(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)
+        """
+        This function scrolls to bottom of the page once.
 
-    def scroll_infinite(self,max_scroll=None):
+        If your webpage is an infinite scroll see function scroll_infinite()
+        """
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+
+    def scroll_infinite(self, max_scroll=None):
+        """
+        This function will scroll infinity to the specified number of scrolls.
+
+        Args:
+            max_scroll (int): The number of scrolls the user would like to perform.
+                (default is None)
+        """
         #ENHANCEMENT: Could ask user number of products per page and the number products they want to calculate the max scroll
+        #ENHANCEMENT: or simply change max_scroll to number of elemnts desired.
         scroll_count = 0
         previous_height = self.driver.execute_script("return document.body.scrollHeight")
         while True:
             # do the scrolling
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-            time.sleep(3)
+            time.sleep(2)
 
             new_height = self.driver.execute_script("return document.body.scrollHeight")
             if new_height == previous_height:
@@ -109,6 +197,18 @@ class Scraper:
                 break
 
     def scrape_page_links(self, xpath, max_scroll=None):
+        """
+        This function will scroll infinity and scrape all of the desired 'href' 
+        elements using the xpath.
+
+        Args:
+            xpath (str): The x path elements to be scraped
+            max_scroll (int): The number of scrolls the user would like to perform.
+                (default is None)
+        Returns:
+            list: a list of strings representing the text for the url for the specified elements
+                    list.
+        """
         scroll_count = 0
         previous_height = self.driver.execute_script("return document.body.scrollHeight")
         while True:
@@ -128,41 +228,59 @@ class Scraper:
         link = [links.get_attribute('href') for links in links]
         return link
 
-    def login(self, username, password, user_xpath, pass_xpath, button_xpath):
-            user_entry = self.driver.find_element(By.XPATH, user_xpath)
-            pass_entry = self.driver.find_element(By.XPATH, pass_xpath)
-            user_entry.send_keys(username)
-            pass_entry.send_keys(password)
-            sign_in_button = self.driver.find_element(By.XPATH, button_xpath)
-            sign_in_button.click()
-
-    # def generate_uid(self):
-    #     parsed = urlparse(self.url)
-    #     query = parse_qs(parsed.query)
-    #     [page] = query['page']
+    # def login(self, username, password, user_xpath, pass_xpath, button_xpath):
+    #         user_entry = self.driver.find_element(By.XPATH, user_xpath)
+    #         pass_entry = self.driver.find_element(By.XPATH, pass_xpath)
+    #         user_entry.send_keys(username)
+    #         pass_entry.send_keys(password)
+    #         sign_in_button = self.driver.find_element(By.XPATH, button_xpath)
+    #         sign_in_button.click()
     
     def generate_uuid4(self):
-        self.uuid_four = uuid.uuid4()
+        """
+        This function will generate a random unquie identifier using the uuid4 format.
+
+        Returns:
+            str: a string representing the random uuid4.
+        """
+        uuid_four = str(uuid.uuid4())
+        return uuid_four
 
     def download_image(self, xpath, file_name):
-        img = self.driver.find_element(By.XPATH, xpath)
-        img = img.get_attribute('src')
-        urllib.request.urlretrieve(img,file_name)
+        """
+        This function will find the 'src' attribute for the image and download
+        the image to a speified file name.
+
+        The download will occur in the root folder for the project. The user can speficy 
+        the directory for the file to be saved by entering the full root path 
+        (including desried file name) as the file_name arg. 
+
+        Args:
+            xpath (str): The x path element to be scraped that includes the 'src' element.
+            file_name (str): The desried file_name.
+        """
+        img = self.driver.find_element(By.XPATH, xpath).get_attribute('src')
+        urllib.request.urlretrieve(img, file_name)
     
-    def create_directory(self,directory_name, directory_path ):
+    def create_directory(self, directory_name, directory_path):
+        """
+        This function create a folder in the directory desired with the name desired.
+
+        Args:
+            directory_name (str): The name for the new directory that will be created.
+            directory_path (str): The path to create the new directory.
+        """
         # identify the root of the path
-        self.directory_name = directory_name
-        self.directory_path = directory_path
-        self.path = os.path.join(self.directory_path, self.directory_name)
+        directory_name = directory_name
+        directory_path = directory_path
+        path = os.path.join(directory_path, directory_name)
         # make the directory in the root of the project
         try:
-            os.makedirs(self.path, exist_ok=True)
+            os.makedirs(path, exist_ok=True)
         except OSError:
-            print ("Creation of the directory %s failed" % self.path)
+            print ("Creation of the directory %s failed" % path)
         else:
-            print ("Successfully created the directory %s " % self.path)
-
-
+            print ("Successfully created the directory %s " % path)
 
 if __name__ == "__main__":
     URL = 'https://www.allrecipes.com/search/results/?search='
@@ -170,21 +288,18 @@ if __name__ == "__main__":
     bot.accept_cookies('//*[@id="onetrust-accept-btn-handler"]',None)
     #bot.navigate_to('//a[@class="card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"]', 'href')
     links = bot.scrape_page_links('//a[@class="card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"]',1)
-    root_path = os.getcwd()
-    bot.create_directory('raw_data', root_path)
+    bot.create_directory('raw_data', bot.get_root_path())
 
     for link in links:
         # ENHANCEMENT: Download multiple images
-        # ENHANCEMENT: Add logic...
         bot.driver.get(link)
         time.sleep(2)
-        recipe_id = [int(''.join(group)) for key, group in groupby(iterable=link, key=lambda e: e.isdigit()) if key]
-        uuid4 = str(bot.generate_uuid4)
+        # recipe_id = [int(''.join(group)) for key, group in groupby(iterable=link, key=lambda e: e.isdigit()) if key]
         recipe_dict = {
-                'recipe_uuid4': [uuid4],
-                'recipe_id': [recipe_id],
-                'link': [link],    
-                 }
+            'recipe_uuid4': [bot.generate_uuid4()],
+            'recipe_id': [bot.extract_continous_digit_group(link)],
+            'link': [link],    
+            }
         scraped_page_dict = bot.scrape_multiple_page_elements(
             ingredient_list='//span[@class="ingredients-item-name elementFont__body"]', 
             recipe_meta='//div[@class="recipe-meta-item"]',
@@ -193,21 +308,12 @@ if __name__ == "__main__":
             nutrition_summary= '//div[@class="section-body"]'
             )
         recipe_dict.update(scraped_page_dict)
-        raw_data_path = os.path.join(root_path,'raw_data')
-        bot.create_directory(str(recipe_id), raw_data_path)
-        recipe_path = os.path.join(raw_data_path, str(recipe_id))
-        with open(os.path.join(recipe_path,'data.json'), mode='w') as f:
-            json.dump(recipe_dict, f)
-        time.sleep(1)
+        raw_data_path = os.path.join(bot.get_root_path(),'raw_data')
+        bot.create_directory(recipe_dict['recipe_id'][0], raw_data_path)
+        recipe_path = os.path.join(raw_data_path, recipe_dict['recipe_id'][0])
+        bot.create_json(recipe_path,'data.json')
         bot.create_directory('images',recipe_path)
         images_path = os.path.join(recipe_path,'images')
         bot.download_image('//div[@class="inner-container js-inner-container image-overlay"]/img', os.path.join(images_path, str(0)))
 
-        # unit testing ideas:
-        # 1. Check that the uuid4 does not duplicate
-        # 2. Check it gets the images for each, could probaly att wait for element to my functions for safety
-        # 3. Add extra element in does it still work
-        # 4. Change website, does it still work
-        # 5. If there is a duplicate id will it error
-        # 6. Does the data scraped match the expected values (foudn it doesn't seem to line 1/4 or brackets like (12 Ounce))
-        # 7. using find elements when there is only one element in the multiple scraper - does it still work?
+
