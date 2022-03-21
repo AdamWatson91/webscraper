926ebd773f22e2ef2282ed61b961f040c6daf4c9
Adam Watson <adamwatson91@gmail.com>
2022-03-21 21:32:28 +0000

    Remove files not required - developer playground and grafana download

--- setup.py
+++ setup.py
@@ -6,8 +6,18 @@ setup(
     version='0.0.1',
     description='A webscraper that allows you to navigate and extract data from websites',
     url='https://github.com/AdamWatson91/webscraper.git',
-    author='Adam Watson',  # Your name
+    author='Adam Watson',
     license='MIT',
     packages=find_packages(),
-    install_requires=['selenium', 'webdriver_manager'],
+    install_requires=['selenium', 
+        'webdriver_manager',
+        'boto3',
+        'pandas',
+        'selenium',
+        'setuptools',
+        'SQLAlchemy',
+        'tqdm',
+        'webdriver_manager',
+        'psycopg2'
+        ]
 )
